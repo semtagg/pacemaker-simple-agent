aa4154c87f700f6dff562fcb552adc2f6383f5cf
semtagg <semtagg@yandex.ru>
2023-03-24 19:29:07 +0300

    wip

--- dlopen/test.c
+++ dlopen/test.c
@@ -4,7 +4,7 @@
 
 #define DLOPEN_SUCCESS             0
 
-void test_write_message(const char* instance, const char* message) {
+static void test_write_message(const char* instance, const char* message) {
     FILE* fptr;
     char path[100];
     snprintf(path, sizeof(path), "/var/log/dlopen/%s.log", instance);
@@ -15,48 +15,43 @@ void test_write_message(const char* instance, const char* message) {
 }
 
 int start(GHashTable* params, char** error) {
-    test_write_message((char *)g_hash_table_lookup(params, "OCF_RESOURCE_INSTANCE"),
+    test_write_message((char *)g_hash_table_lookup(params, "DLOPEN_RESOURCE_INSTANCE"),
                        "Action: start, instance: %s.\n");
     return DLOPEN_SUCCESS;
 }
 
 int stop(GHashTable* params, char** error) {
-    test_write_message((char *)g_hash_table_lookup(params, "OCF_RESOURCE_INSTANCE"),
+    test_write_message((char *)g_hash_table_lookup(params, "DLOPEN_RESOURCE_INSTANCE"),
                        "Action: stop, instance: %s.\n");
     return DLOPEN_SUCCESS;
 }
 
 int status(GHashTable* params, char** error) {
-    test_write_message((char *)g_hash_table_lookup(params, "OCF_RESOURCE_INSTANCE"),
+    test_write_message((char *)g_hash_table_lookup(params, "DLOPEN_RESOURCE_INSTANCE"),
                        "Action: status, instance: %s.\n");
     return DLOPEN_SUCCESS;
 }
 
 int monitor(GHashTable* params, char** error) {
-    test_write_message((char *)g_hash_table_lookup(params, "OCF_RESOURCE_INSTANCE"),
+    test_write_message((char *)g_hash_table_lookup(params, "DLOPEN_RESOURCE_INSTANCE"),
                        "Action: monitor, instance: %s.\n");
     return DLOPEN_SUCCESS;
 }
 
-int metadata(char** stdout_data) {
-    static const char meta_data[] = 
-    "<?xml version=\"1.0\"?>\n"
-    "<!DOCTYPE resource-agent SYSTEM \"ra-api-1.dtd\">\n"
-    "<resource-agent name=\"libtest.so\" version=\"1.0\">\n"
-    "  <version>1.0</version>\n"
-    "  <longdesc lang=\"en\">\n"
-    "   This script is used to write data to a file.\n"
-    "  </longdesc>\n"
-    "  <shortdesc lang=\"en\">Test agent</shortdesc>\n"
-    "  <actions>\n"
-    "    <action name=\"start\"   timeout=\"15s\" />\n"
-    "    <action name=\"stop\"    timeout=\"15s\" />\n"
-    "    <action name=\"status\"  timeout=\"15s\" interval=\"15s\" />\n"
-    "    <action name=\"monitor\" timeout=\"15s\" interval=\"15s\" />\n"
-    "    <action name=\"meta-data\"  timeout=\"5s\" />\n"
-    "  </actions>\n"
-    "</resource-agent>\n";
-
-    *stdout_data = strdup(meta_data);
-    return DLOPEN_SUCCESS;
-}
+const char metadata[] = 
+"<?xml version=\"1.0\"?>\n"
+"<!DOCTYPE resource-agent SYSTEM \"ra-api-1.dtd\">\n"
+"<resource-agent name=\"libtest.so\" version=\"1.0\">\n"
+"  <version>1.0</version>\n"
+"  <longdesc lang=\"en\">\n"
+"   This script is used to write data to a file.\n"
+"  </longdesc>\n"
+"  <shortdesc lang=\"en\">Test agent</shortdesc>\n"
+"  <actions>\n"
+"    <action name=\"start\"   timeout=\"15s\" />\n"
+"    <action name=\"stop\"    timeout=\"15s\" />\n"
+"    <action name=\"status\"  timeout=\"15s\" interval=\"15s\" />\n"
+"    <action name=\"monitor\" timeout=\"15s\" interval=\"15s\" />\n"
+"    <action name=\"meta-data\"  timeout=\"5s\" />\n"
+"  </actions>\n"
+"</resource-agent>\n";
